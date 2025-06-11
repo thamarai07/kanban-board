720f66bbf9993272ffb4b865067eea4030ef80d5
thamaraikannan <thamaraik69@gmail.com>
2025-06-11 17:41:54 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,12 +15,7 @@ export default function Navbar() {
               View Task
             </Link>
           </li>
-          <li className="flex gap-10 w-[40%]">
-            <input className="w-[100%] py-2 rounded px-2 border-2" />
-            <button className="bg-blue-500 text-white px-4 rounded">
-              Search
-            </button>
-          </li>
+          
         </ul>
       </div>
     </nav>
